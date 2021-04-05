471b57386ca3b2288de6f98175ffd7843b937518
Fernando Pascoal <fernando.pascoal42@gmail.com>
2021-04-04 21:28:09 -0300

    Rebuild e clean.

---
+++ Lista_Exercicios_1/04_trocar_fragment_shader/04_trocar_fragment_shader.cpp
@@ -0,0 +1,12 @@
+/* 
+4. Utilize qualquer dos exemplos dados em sala, que contenham shaders,
+e modifique os códigos afim de trocar um fragment shader por outro (ou seja, substituir um shader por outro)
+sem reiniciar a sua aplicação.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
